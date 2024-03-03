bdfe5b41fe3163fb676ada96736c4bc6bb3b005a
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-03 17:36:03 -0500

    updated file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './Cards.js';
+import './Navbar.js';
 function App() {
   return (
     <div className="App">
+
+<Navbar />
        <h1>CS 230L</h1>
        <h2>Section - 001</h2>
        <p>WVU ID: 800288188</p>
        <p>Hi I am Arjun Vijay</p>
+       <Cards />
+       <Cards />
+       <Cards />
        </div>
+  
   );
 }
 
