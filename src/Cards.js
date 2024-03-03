e27c339c79e9a29a1dd4d84d48a69099e81d8fc8
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-03 18:17:41 -0500

    new file

--- src/Cards.js
+++ src/Cards.js
@@ -1,6 +1,12 @@
-const Card = ({ title, content }) => {
+const Card = ({ title, content, color }) => {
+  const cardStyle = {
+    width: '18rem',
+    margin: '10px',
+    backgroundColor: color || 'white', // Use the specified color or default to white
+  };
+
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card" style={cardStyle}>
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{content}</p>
