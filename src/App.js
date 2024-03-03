6602b36fa3db7d90f3d83e81d4814841ca2fe0b4
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-03 18:00:51 -0500

    newer file

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ function App() {
        <h2>Section - 001</h2>
        <p>WVU ID: 800288188</p>
        <p>Hi I am Arjun Vijay</p>
-       <Cards />
-       <Cards />
-       <Cards />
+       <div className="card-container">
+        <Card title="Card 01" content="Some quick example text to build on the card title and make up the bulk of the card's content" />
+        <Card title="Card 02" content="Some quick example text to build on the card title and make up the bulk of the card's content" />
+        <Card title="Card 03" content="Some quick example text to build on the card title and make up the bulk of the card's content" />
+      </div>
        </div>
   
   );
