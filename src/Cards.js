271c0dcff10ebd9cceccf22afd048fbca50866f6
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-03 17:55:08 -0500

    new file

--- src/Cards.js
+++ src/Cards.js
@@ -1,8 +1,12 @@
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+const Card = ({ title, content }) => {
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
