358d7c1c502bb28ad61fcf7199be3b4dbc05887c
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-03 18:53:35 -0500

    new file

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-end">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -18,7 +18,7 @@ const Navbar = () => {
             <a className="nav-link" href="#">Content</a>
           </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
+        <form className="form-inline ml-auto my-2 my-lg-0">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
