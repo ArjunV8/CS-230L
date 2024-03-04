71aa686c870ed505ffc767ea0b8ac76570d7097e
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-03 19:00:09 -0500

    newest file

--- src/Navbar.js
+++ src/Navbar.js
@@ -19,8 +19,10 @@ const Navbar = () => {
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0" style={{ marginLeft: 'auto' }}>
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <div className="d-flex">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </div>
         </form>
       </div>
     </nav>
